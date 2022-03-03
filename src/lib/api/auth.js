ed9558be0d8adec4efc84d23748b2deaf6be5fa6
do-mandoo <67843356+do-mandoo@users.noreply.github.com>
2022-03-03 18:47:38 +0900

    api설정

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -4,10 +4,10 @@ export const client = axios.create({ baseURL: 'http://localhost:5001/api' });
 
 // // 로그인
 // export const login = ({ username, password }) =>
-//   client.post('/api/users/login', { username, password });
+//   client.post('/auth/login', { username, password });
 
 // // 회원가입
-// export const register = ({ username, password }) =>
-//   client.post('/api/users/register', { username, password });
+// export const register = ({ username, nickname, password }) =>
+//   client.post('/auth/register', { username, nickname, password });
 
-// // 로그인 상태 확인(보류)
+// 로그인 상태 확인(보류)
