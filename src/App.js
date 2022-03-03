0b7694f2dd148164e1233f5f0e42af505eabf190
do-mandoo <67843356+do-mandoo@users.noreply.github.com>
2022-03-03 18:48:49 +0900

    contextapiProvider설정

--- src/App.js
+++ src/App.js
@@ -3,25 +3,34 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import PostLists from './components/posts/PostLists';
 import { AuthReducer, AuthInitialState } from './contexts/auth';
-import { Auth } from './contexts/store';
+import { Auth, Write } from './contexts/store';
+import { WriteInitialState, WriteReducer } from './contexts/write';
 import LoginPage from './pages/LoginPage';
 import PostListPage from './pages/PostListPage';
 import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
 import TextArea from './testArea/TextArea';
 
 function App() {
   const [AuthState, AuthDispatch] = useReducer(AuthReducer, AuthInitialState);
+  const [WriteState, WriteDispatch] = useReducer(
+    WriteReducer,
+    WriteInitialState
+  );
   return (
     <>
       {/* <TextArea /> */}
-      <Auth.Provider value={{ AuthState, AuthDispatch }}>
-        <Switch>
-          <Route path="/login" component={LoginPage} />
-          <Route path="/register" component={RegisterPage} />
-          <Route path="/post" component={PostLists} />
-          <Route path="/" component={PostListPage} />
-        </Switch>{' '}
-      </Auth.Provider>
+      <Write.Provider value={{ WriteState, WriteDispatch }}>
+        <Auth.Provider value={{ AuthState, AuthDispatch }}>
+          <Switch>
+            <Route path="/login" component={LoginPage} />
+            <Route path="/register" component={RegisterPage} />
+            <Route path="/post" component={PostLists} />
+            <Route path="/write" component={WritePage} />
+            <Route path="/" component={PostListPage} />
+          </Switch>
+        </Auth.Provider>
+      </Write.Provider>
     </>
   );
 }
